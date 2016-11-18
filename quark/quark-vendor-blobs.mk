70aff6c1776175924499394940440b1211b0acbb
Felipe Leon <fglfgl27@gmail.com>
2016-11-18 19:59:59 -0200

    Quark: correct some blobs version
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- quark/quark-vendor-blobs.mk
+++ quark/quark-vendor-blobs.mk
@@ -191,6 +191,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/quark/proprietary/vendor/lib/libthermalioctl.so:system/vendor/lib/libthermalioctl.so \
     vendor/motorola/quark/proprietary/bin/qseecomd:system/bin/qseecomd \
     vendor/motorola/quark/proprietary/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+    vendor/motorola/quark/proprietary/lib/liboemcrypto.so:system/lib/liboemcrypto.so \
     vendor/motorola/quark/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     vendor/motorola/quark/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/motorola/quark/proprietary/vendor/lib/libdrmfs.so:system/vendor/lib/libdrmfs.so \
@@ -385,4 +386,5 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/quark/proprietary/etc/sp/sp_lib:system/etc/sp/sp_lib \
     vendor/motorola/quark/proprietary/etc/sp/sp_bin:system/etc/sp/sp_bin \
     vendor/motorola/quark/proprietary/bin/ims_rtp_daemon:system/bin/ims_rtp_daemon \
-    vendor/motorola/quark/proprietary/vendor/lib/libWVphoneAPI.so:system/vendor/lib/libWVphoneAPI.so
+    vendor/motorola/quark/proprietary/etc/permissions/com.verizon.hardware.telephony.ehrpd.xml:system/etc/permissions/com.verizon.hardware.telephony.ehrpd.xml \
+    vendor/motorola/quark/proprietary/etc/permissions/com.verizon.hardware.telephony.lte.xml:system/etc/permissions/com.verizon.hardware.telephony.lte.xml
