3daa6f958995c2a0d0c4f2d73a63fb3a3d8e568a
Felipe Leon <fglfgl27@gmail.com>
2016-11-25 01:27:24 -0200

    Quark: Update qcom thermal and camera bin and libs + mpdecision
    Latest shamu-nbd91p
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- quark/quark-vendor-blobs.mk
+++ quark/quark-vendor-blobs.mk
@@ -387,4 +387,5 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/quark/proprietary/etc/sp/sp_bin:system/etc/sp/sp_bin \
     vendor/motorola/quark/proprietary/bin/ims_rtp_daemon:system/bin/ims_rtp_daemon \
     vendor/motorola/quark/proprietary/etc/permissions/com.verizon.hardware.telephony.ehrpd.xml:system/etc/permissions/com.verizon.hardware.telephony.ehrpd.xml \
-    vendor/motorola/quark/proprietary/etc/permissions/com.verizon.hardware.telephony.lte.xml:system/etc/permissions/com.verizon.hardware.telephony.lte.xml
+    vendor/motorola/quark/proprietary/etc/permissions/com.verizon.hardware.telephony.lte.xml:system/etc/permissions/com.verizon.hardware.telephony.lte.xml \
+    vendor/motorola/quark/proprietary/vendor/lib/libmmqjpegdma.so:system/vendor/lib/libmmqjpegdma.so
